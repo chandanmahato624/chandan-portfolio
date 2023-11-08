a9a84c2e93fc6cf0d67d1b8e5ade59da2cd1e708
chandan <chandanmahato624@gmail.com>
2023-11-08 20:44:20 +0530

    Contact form done with mail link and updated css

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,32 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_6i8xeby','template_tux89lc','#contact-form','7VepERQp_1mG5IA-3')
+    .then(() =>{
+        //show send message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+           // Remove message after five seconds
+           setTimeout(() =>{
+            contactMessage.textContent = ''
+           }, 5000)
+
+           // Clear input fields
+           contactForm.reset()
+    }, () =>{
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
